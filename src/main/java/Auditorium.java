081611fc994874120279cd791eee3f3e04282af3
Akash Sharma S <present.akash@gmail.com>
2017-08-07 22:22:40 +0530

    Learnt about the init-method and destroy-method. The same as finalize, it looks like destroy-method is called only when the GC destroys the spring container? I didn't see the methods get executed in my small app runs so far. Anyway, it looks like initializing multiple containers also instantiates all the classes that are available in the xml file. Should be careful about it.

---
+++ src/main/java/Auditorium.java
@@ -0,0 +1,12 @@
+/**
+ * Created by akash on 07/08/17.
+ */
+public class Auditorium {
+    public void turnOnLights() {
+        System.out.println("Turning on the lights.");
+    }
+
+    public void turnOffLights() {
+        System.out.println("Turning off the lights.");
+    }
+}
